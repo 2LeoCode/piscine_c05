6be7091d51f72d1ec92a840571ab27e7f6f86820
Your Name <you@example.co>
2020-02-13 19:30:17 +0100

    sqrt recur

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -6,20 +6,24 @@
 /*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 10:17:44 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/02/13 10:25:25 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/02/13 19:22:37 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int		ft_recurs_sqrt(int i, int nbr)
+{
+	if (i * i == nbr)
+		return(i);
+	else if (i <= 46340)
+		return(ft_recurs_sqrt(i + 1, nbr));
+	else
+		return(0);
+}
+
 int		ft_sqrt(int nb)
 {
-	int i;
-
-	i = -1;
-	if (nb < 0)
+	if (nb <= 0)
 		return (0);
-	while (++i < nb){
-		if(i * i == nb)
-			return (i);
-	}
-	return (0);
+	else
+		return (ft_recurs_sqrt(0, nb));
 }
